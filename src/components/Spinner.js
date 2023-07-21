71e37935695669c749099a7653983828d23b6db9
Prince <ranap8348@gmail.com>
2023-07-21 15:40:29 +0530

    bug fixes

---
+++ src/components/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import loading from './loading2.gif';
+export default function Spinner() {
+  return (
+    <div className='text-center'>
+        <img src={loading} alt="Loading" />
+    </div>
+  )
+}
